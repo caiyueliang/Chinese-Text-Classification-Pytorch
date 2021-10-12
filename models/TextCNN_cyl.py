e5ae3338b0beb8a60e4fb45a5606073dc8792f50
caiyueliang <393900414@qq.com>
2021-10-12 16:44:08 +0800

    update

---
+++ models/TextCNN_cyl.py
@@ -0,0 +1,93 @@
+# coding: UTF-8
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset, embedding):
+        self.model_name = 'TextCNN_cyl'
+
+        self.train_path = os.path.join(dataset, 'train.pt')                 # 训练集
+        self.dev_path = os.path.join(dataset, 'dev.pt')                     # 验证集
+        self.test_path = os.path.join(dataset, 'test.pt')                   # 测试集
+
+        self.class_list = [x.strip() for x in open(
+            os.path.join(dataset, 'class.txt'), encoding='utf-8').readlines()]          # 类别名单
+
+        # self.vocab_path = dataset + '/data/vocab.pkl'                                 # 词表
+
+        self.save_dir = os.path.join(dataset, 'save_models')
+        os.makedirs(self.save_dir, exist_ok=True)
+        self.save_path = os.path.join(self.save_dir, self.model_name + '.ckpt')         # 模型训练结果
+
+        self.log_path = os.path.join(dataset, 'log', self.model_name)
+
+        # self.embedding_pretrained = torch.tensor(
+        #     np.load(dataset + '/data/' + embedding)["embeddings"].astype('float32')) \
+        #     if embedding != 'random' else None                                        # 预训练词向量
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')      # 设备
+
+        self.dropout = 0.5                                              # 随机失活
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        # self.n_vocab = 0                                              # 词表大小，在运行时赋值
+        self.num_epochs = 20                                            # epoch数
+        self.batch_size = 128                                           # mini-batch大小
+        # self.pad_size = 32                                            # 每句话处理成的长度(短填长切)
+        self.learning_rate = 1e-3                                       # 学习率
+
+        # self.embed = self.embedding_pretrained.size(1)\
+        #     if self.embedding_pretrained is not None else 300         # 字向量维度
+        self.embed = 30
+
+        self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
+        self.num_filters = 256                                          # 卷积核数量(channels数)
+
+
+'''Convolutional Neural Networks for Sentence Classification'''
+
+
+class Model(nn.Module):
+    def __init__(self, config):
+        super(Model, self).__init__()
+
+        # if config.embedding_pretrained is not None:
+        #     self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
+        # else:
+        #     self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab - 1)
+
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(1, config.num_filters, (k, config.embed)) for k in config.filter_sizes])
+        self.dropout = nn.Dropout(config.dropout)
+        self.fc = nn.Linear(config.num_filters * len(config.filter_sizes), config.num_classes)
+
+    def conv_and_pool(self, x, conv):
+        x = F.relu(conv(x)).squeeze(3)
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        return x
+
+    def forward(self, x):
+        print(" =============================== ")
+        # x, _ = x                          # x是一个tuple, 这边只用了第一个元素，即文本的索引。[batch_size, seq_len] = [128, 32]
+        print("[TextCNN] x: {}".format(x.size()))
+
+        out = x
+        # out = self.embedding(x)
+        print("[TextCNN] after [embedding]: {}".format(out.size()))
+
+        out = out.unsqueeze(1)              # 多一维: [batch_size, 1, seq_len] = [128, 1, 32]
+        print("[TextCNN] after [unsqueeze]: {}".format(out.size()))
+        out = torch.cat([self.conv_and_pool(out, conv) for conv in self.convs], 1)
+        print("[TextCNN] after [conv_and_pool]: {}".format(out.size()))
+        out = self.dropout(out)
+        print("[TextCNN] after [dropout]: {}".format(out.size()))
+        out = self.fc(out)
+        print("[TextCNN] after [fc]: {}".format(out.size()))
+
+        return out
--- run.py
+++ run.py
@@ -10,20 +10,27 @@ parser = argparse.ArgumentParser(description='Chinese Text Classification')
 parser.add_argument('--model', type=str, required=True, default='TextCNN', help='choose a model: TextCNN, TextRNN, FastText, TextRCNN, TextRNN_Att, DPCNN, Transformer')
 parser.add_argument('--embedding', default='pre_trained', type=str, help='random or pre_trained')
 parser.add_argument('--word', default=False, type=bool, help='True for word, False for char')
+# parser.add_argument('--dataset', default="THUCNews")
+parser.add_argument('--dataset', default="./data_train/v1.0.0/")
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    dataset = 'THUCNews'  # 数据集
+    # dataset = 'THUCNews'  # 数据集
+    dataset = args.dataset
 
     # 搜狗新闻:embedding_SougouNews.npz, 腾讯:embedding_Tencent.npz, 随机初始化:random
     embedding = 'embedding_SougouNews.npz'
+
     if args.embedding == 'random':
         embedding = 'random'
     model_name = args.model  # 'TextRCNN'  # TextCNN, TextRNN, FastText, TextRCNN, TextRNN_Att, DPCNN, Transformer
+
     if model_name == 'FastText':
         from utils_fasttext import build_dataset, build_iterator, get_time_dif
         embedding = 'random'
+    elif model_name == 'TextCNN_cyl' or model_name == 'TextRNN_cyl':
+        from utils_fasttext_cyl import build_dataset, build_iterator, get_time_dif
     else:
         from utils import build_dataset, build_iterator, get_time_dif
 

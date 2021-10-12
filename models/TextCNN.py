6709001288a287178e232a3b5ab184b9eb5cc11e
caiyueliang <393900414@qq.com>
2021-10-12 15:55:17 +0800

    update

--- models/TextCNN.py
+++ models/TextCNN.py
@@ -28,11 +28,13 @@ class Config(object):
         self.num_classes = len(self.class_list)                         # 类别数
         self.n_vocab = 0                                                # 词表大小，在运行时赋值
         self.num_epochs = 20                                            # epoch数
-        self.batch_size = 8                                           # mini-batch大小
+        self.batch_size = 8                                             # mini-batch大小
         self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
         self.learning_rate = 1e-3                                       # 学习率
+
         self.embed = self.embedding_pretrained.size(1)\
             if self.embedding_pretrained is not None else 300           # 字向量维度
+
         self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
         self.num_filters = 256                                          # 卷积核数量(channels数)
 
@@ -43,10 +45,12 @@ class Config(object):
 class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
+
         if config.embedding_pretrained is not None:
             self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
         else:
             self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab - 1)
+
         self.convs = nn.ModuleList(
             [nn.Conv2d(1, config.num_filters, (k, config.embed)) for k in config.filter_sizes])
         self.dropout = nn.Dropout(config.dropout)

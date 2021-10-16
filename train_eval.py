16ffe03be211232f7675bfaadd695792c089ee43
caiyueliang <393900414@qq.com>
2021-10-16 16:47:19 +0800

    update

--- train_eval.py
+++ train_eval.py
@@ -93,16 +93,18 @@ def test(config, model, test_iter):
     # test
     model.load_state_dict(torch.load(config.save_path))
     model.eval()
-    start_time = time.time()
+    # start_time = time.time()
     test_acc, test_loss, test_report, test_confusion = evaluate(config, model, test_iter, test=True)
-    msg = 'Test Loss: {0:>5.2},  Test Acc: {1:>6.2%}'
+    msg = '[test] Test Loss: {0:>5.2},  Test Acc: {1:>6.2%}'
+    print("===================================================")
     print(msg.format(test_loss, test_acc))
     print("Precision, Recall and F1-Score...")
     print(test_report)
     print("Confusion Matrix...")
     print(test_confusion)
-    time_dif = get_time_dif(start_time)
-    print("Time usage:", time_dif)
+    print("===================================================")
+    # time_dif = get_time_dif(start_time)
+    # print("Time usage:", time_dif)
 
 
 def evaluate(config, model, data_iter, test=False):
